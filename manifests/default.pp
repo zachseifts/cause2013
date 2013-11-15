9212ad8ea5a1d97d095401a9883d0742f81f05ad
Zach Seifts <zach.seifts@gmail.com>
2013-11-15 14:03:35 -0500

    Adding in php, php-pear, php-console-table, drush, and host entry.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,27 @@
 node default {
+    package { php5:
+        ensure => latest,
+    }
+
+    package { php-pear:
+        ensure => latest,
+        require => Package['php5']
+    }
+
+    package { php-console-table:
+        ensure => latest,
+        require => [Package['php5'], Package['php-pear']]
+    }
+
+    exec { 'install drush':
+        command => '/usr/bin/pear channel-discover pear.drush.org && /usr/bin/pear install drush/drush',
+        require => Package['php-console-table'],
+        creates => '/usr/bin/drush'
+    }
+
+    host { 'test.cause13.local':
+        ensure => 'present',
+        ip => '127.0.0.1'
+    }
 }
 
