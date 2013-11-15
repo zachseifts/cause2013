7b5301d2b36b6569746bfc07d4fd7cd6fd5a1b67
Zach Seifts <zach.seifts@gmail.com>
2013-11-15 14:26:25 -0500

    Adding in modules and building lamp stack

--- manifests/default.pp
+++ manifests/default.pp
@@ -23,5 +23,34 @@ node default {
         ensure => 'present',
         ip => '127.0.0.1'
     }
+
+    class { 'apache':
+        default_vhost => false,
+        mpm_module => 'prefork',
+    }
+
+    class { 'apache::mod::php':
+        require => Package["php5"]
+    }
+
+    class { '::mysql::server':
+        root_password => 'root',
+    }
+
+    apache::vhost { 'test.cause13.local':
+        priority => 10,
+        port => 80,
+        docroot => '/var/www/test.cause13.local',
+        docroot_owner => 'www-data',
+        docroot_group => 'www-data',
+    }
+
+    file { '/var/www/test.cause13.local/index.php':
+        ensure => 'present',
+        content => '<?php phpinfo(); ?>',
+        mode => 0664,
+        owner => 'www-data',
+        group => 'www-data',
+    }
 }
 
